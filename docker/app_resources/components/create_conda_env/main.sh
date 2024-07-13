d86863e7d2d1f6e674e638742369d18a19f2dad3
Duane Goodner <dmgoodner@gmail.com>
2024-07-13 08:34:40 -0600

    remove --force from mamba command (no valid w/ current mamba)

--- docker/app_resources/components/create_conda_env/main.sh
+++ docker/app_resources/components/create_conda_env/main.sh
@@ -8,7 +8,7 @@ conda_dir=$4
 
 sudo mkdir "$env_path"
 sudo chown "$user":"$user" "$env_path"
-mamba env create --prefix "$env_path" --file "$env_yml" --force
+mamba env create --prefix "$env_path" --file "$env_yml"
 mamba clean --all --yes --force-pkgs-dirs
 find "$conda_dir" -follow -type f -name '*.a' -delete
 find "$conda_dir" -follow -type f -name '*.pyc' -delete
