9f94603b82c0fd19a7fa44713b3f83002c96fd5d
Duane Goodner <dmgoodner@gmail.com>
2024-08-01 19:39:42 -0600

    Move root level .py files to appropriate sub dirs

---
+++ src/lstm_adversarial_attack/query_db/test_mimiciii_db.py
@@ -0,0 +1,28 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent.parent))
+from lstm_adversarial_attack.config import CONFIG_READER
+import lstm_adversarial_attack.query_db.mimiciii_database as mdb
+
+def test_mimiciii_db_connection():
+    db_access = mdb.MimiciiiDatabaseAccess(
+        dotenv_path=Path(CONFIG_READER.read_path(config_key="db.mimiciii_dotenv")),
+        output_parent=Path(CONFIG_READER.read_path(config_key="db.output_root")),
+    )
+
+    try:
+        db_access.connect()
+        print("Connected to MIMIC-III database")
+    except Exception as e:
+        print(e)
+
+    try:
+        db_access.close_connection()
+        print("Connection to MIMIC-III database closed")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    test_mimiciii_db_connection()
