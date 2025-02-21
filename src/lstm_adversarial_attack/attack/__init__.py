454c78f5e7b0eb5ebda7e27fdc7f532889d4d708
Duane Goodner <dmgoodner@gmail.com>
2025-02-21 14:32:43 -0700

    Update sub-package __init__.py docs

---
+++ src/lstm_adversarial_attack/attack/__init__.py
@@ -0,0 +1,3 @@
+"""
+Tunes and trains an adversarial attack model, and  uses trained attacker.
+"""
---
+++ src/lstm_adversarial_attack/attack_analysis/__init__.py
@@ -0,0 +1,3 @@
+"""
+Organizes and analyzes adversarial attack results, characterizing distributions of discovered adversarial examples.
+"""
